b45f43e188d8a8cd5ab390e367973c28eda8f477
Josephine-gith <josephine.peronne@gmail.com>
2025-03-11 16:31:24 +0100

    chiffres dans console

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-// NOTE: 
+// NOTE: Joséphine Péronne
 // This is the starter file for a blog post "How to build a calculator". You can follow the lesson at https://zellwk.com/blog/calculator-part-1
 
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
+
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+
+keys.addEventListener('click', e => {
+  console.log(e.target)
+  }
+)
+
+
