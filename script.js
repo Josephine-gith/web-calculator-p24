53585bde2e85eabe7b6c218de47a7c07be35fa03
Josephine-gith <josephine.peronne@gmail.com>
2025-03-11 16:40:07 +0100

    toutes les touches dans console

--- script.js
+++ script.js
@@ -8,7 +8,31 @@ const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator__keys')
 
 keys.addEventListener('click', e => {
-  console.log(e.target)
+    const key = e.target
+    const action = key.dataset.action
+
+    if (!action) {
+        console.log('number key :', key)
+    }
+    if (
+        action === 'add' ||
+        action === 'subtract' ||
+        action === 'multiply' ||
+        action === 'divide'
+      ) {
+        console.log('operator key :', action)
+    }
+    if (action === 'decimal') {
+        console.log('decimal key!')
+      }
+      
+      if (action === 'clear') {
+        console.log('clear key!')
+      }
+      
+      if (action === 'calculate') {
+        console.log('equal key!')
+    }
   }
 )
 
