379b093b5d3d7ad688e626c916f08919a9df8ff2
Josephine-gith <josephine.peronne@gmail.com>
2025-04-01 00:34:53 +0200

    ça calcule

--- script.js
+++ script.js
@@ -7,12 +7,26 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calculator__keys')
 const display = calculator.querySelector('.calculator__display')
+let valeurRecup=0
+let operatorRecup = "add"
 
 keys.addEventListener('click', e => {
-    if (e.target.matches('button')){    
-    console.log(e.target.textContent)
-    //let valeurRecup = display.textContent
-    document.querySelector('.calculator__display').textContent = e.target.textContent
+    if (e.target.matches('button')){
+        console.log(valeurRecup)
+    if (e.target.classList.contains("key--operator")){
+        console.log(e.target.dataset.action)
+        operatorRecup = e.target.dataset.action
+        display.textContent = e.target.textContent
+    }
+    else{
+        valeur = Number(e.target.textContent)
+        if(operatorRecup=="add"){valeurRecup = valeurRecup + valeur
+        }
+        if(operatorRecup=="divide"){valeurRecup = valeurRecup / valeur}
+        if(operatorRecup=="multiply"){valeurRecup = valeurRecup * valeur}
+        if(operatorRecup=="substract"){valeurRecup = valeurRecup - valeur}
+        document.querySelector('.calculator__display').textContent = valeurRecup
+    }
     }
 }
 )
